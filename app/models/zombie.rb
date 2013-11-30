0bcb9d585b531af6141a5165bc90f18d62bac11c
Louis Angelini <louisangelini@gmail.com>
2013-11-30 13:23:21 +0000

    Add rotting and email to Zombie

--- app/models/zombie.rb
+++ app/models/zombie.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: zombies
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  bio        :text
+#  age        :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  email      :string(255)
+#  rotting    :boolean
+#
+
 class Zombie < ActiveRecord::Base
   attr_accessible :age, :bio, :name
 end
--- test/unit/zombie_test.rb
+++ test/unit/zombie_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: zombies
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  bio        :text
+#  age        :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  email      :string(255)
+#  rotting    :boolean
+#
+
 require 'test_helper'
 
 class ZombieTest < ActiveSupport::TestCase
